4b1ae7d8ca35ba7d37cacb9447d5c01e91be1dd5
sock-puppet-dev <notyoursockpuppet@proton.me>
2026-04-16 18:02:34 +0300

    - правки и систематизация шаблонов - использую root-level fallback вместо explicit fallback layer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   content: [
-    './layouts/**/*.gohtml', // теперь Tailwind видит все gohtml шаблоны
-    './content/**/*.md',     // Markdown файлы с HTML-классами Tailwind
-    './assets/**/*.js'       // JS файлы с динамическими классами Tailwind
+    './layouts/**/*.{html,gohtml}', // теперь Tailwind видит все html,gohtml шаблоны
+    './content/**/*.md',            // Markdown файлы с HTML-классами Tailwind
+    './assets/**/*.js',             // JS файлы с динамическими классами Tailwind
   ],
   theme: {
     extend: {},
